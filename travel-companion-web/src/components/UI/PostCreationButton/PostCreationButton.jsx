1f1b5a00a80f7b02c75eba6bf311aa846ddb9c52
Roman Gurinovich <gurinovi4rm@gmail.com>
2024-01-10 17:20:05 +0300

    map ready

--- travel-companion-web/src/components/UI/PostCreationButton/PostCreationButton.jsx
+++ travel-companion-web/src/components/UI/PostCreationButton/PostCreationButton.jsx
@@ -3,7 +3,6 @@ import classes from "./PostCreationButton.module.css"
 
 const PostCreationButton = ({setVisible}) => {
     return (
-
         <div onClick={() => setVisible(true)} className={classes.floatingButton}>
             <img className={classes.icon} src="https://raw.githubusercontent.com/Iconscout/unicons/9a9c624319dc55170e3a1bdee83618c37844f485/svg/line/plus-circle.svg" />
         </div>
--- travel-companion-web/src/components/map/MyMap.jsx
+++ travel-companion-web/src/components/map/MyMap.jsx
@@ -1,33 +1,72 @@
-import React, {useState} from 'react';
-import {FullscreenControl, Map, RouteButton, RouteEditor, YMaps, ZoomControl} from "@pbe/react-yandex-maps";
+import React, { useRef, useState } from 'react';
+import { Map, RouteButton, YMaps, ZoomControl } from "@pbe/react-yandex-maps";
 
+const mapState = { center: [53.489453, 29.341762], zoom: 7 };
 
 const MyMap = () => {
+    const [ymaps, setYmaps] = useState(null);
+    const routes = useRef(null);
 
+    const getRoute = ref => {
+        if (ymaps) {
+            const multiRoute = new ymaps.multiRouter.MultiRoute(
+                {
+                    referencePoints: [[53.489453, 29.341762], [53.902735, 27.555696]],
+                    params: {
+                        results: 2
+                    }
+                },
+                {
+                    boundsAutoApply: true,
+                    routeActiveStrokeWidth: 6,
+                    routeActiveStrokeColor: "#fa6600"
+                }
+            );
+
+            routes.current = multiRoute;
+            ref.geoObjects.add(multiRoute);
+
+            // Устанавливаем targetPoint для RouteButton
+            const routePanel = new ymaps.control.RoutePanel({
+                options: {
+                    size: 'small',
+                    maxWidth: [30, 400, 600],
+                    float: 'right',
+                    routePanelMaxMapArea: 600,
+                    preventDragUpdate: false,
+                }
+            });
+
+            routePanel.routePanel.options.set('types', { masstransit: true, pedestrian: true });
+
+            routePanel.routePanel.state.set({
+                type: 'masstransit',
+                fromEnabled: true,
+                from: [53.489453, 29.341762],
+                to: [53.902735, 27.555696],
+                toEnabled: true,
+            });
+
+
+            ref.controls.add(routePanel);
+        }
+    };
 
     return (
-        <div style={{marginTop:'100px'}}>
-            <YMaps id='map' query={{ apikey: 'd2233e5f-264a-47d7-9fb4-7882b1a56c6c',
-                suggest_apikey: '2f7a2e8c-1916-4656-ba2f-d79a198a1ad2'}}>
+        <div style={{ marginTop: '100px' }} className="App">
+            <YMaps query={{ apikey: 'd2233e5f-264a-47d7-9fb4-7882b1a56c6c', suggest_apikey: '2f7a2e8c-1916-4656-ba2f-d79a198a1ad2' }}>
                 <Map
-                    defaultState={{
-                        center: [53.902284, 27.561831],
-                        zoom: 9,
-                        controls: [],
-                    }}
+                    modules={["multiRouter.MultiRoute", "control.RouteButton", "control.RoutePanel"]}
+                    onLoad={ymaps => setYmaps(ymaps)}
+                    state={mapState}
+                    instanceRef={ref => ref && getRoute(ref)}
                     width='1000px'
                     height='500px'
                 >
-                    <RouteButton instanceRef={(map) => {
-                        console.log(map)
-                    }
-                    } options={{ float: "right" }} />
-                    <RouteEditor />
-                    <ZoomControl options={{ float: "right" }} />
+                    <ZoomControl />
                 </Map>
-            </YMaps>;
+            </YMaps>
         </div>
-
     );
 };
 
